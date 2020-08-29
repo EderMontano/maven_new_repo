9edfb037ca037439a9eeffb33e398854d2ce80b1
EderMontano <eder.montano.ramos@gmail.com>
2020-08-29 11:49:58 -0500

    pom y test

---
+++ src/test/java/testGetHello.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class testGetHello {
+    @Test
+    public void testHello(){
+        assertEquals("Hello world", new Hello().getHello());
+    }
+}
